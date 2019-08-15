2c304340977fa9a00c2d112ee7a7a6b286d01c3f
LorhanSohaky <lorhansohaky@gmail.com>
2019-08-15 00:51:07 -0300

    Usando visitor

--- trabalho1/generateGrammar.sh
+++ trabalho1/generateGrammar.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-java -jar libs/antlr-4.7.2-complete.jar -package la.linguagem.ANTLR AnalisadorLA/src/la/linguagem/ANTLR/la.g4
+java -jar libs/antlr-4.7.2-complete.jar -package la.linguagem.ANTLR AnalisadorLA/src/la/linguagem/ANTLR/la.g4 -visitor
