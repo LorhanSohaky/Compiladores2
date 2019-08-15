d795e48a98d7802813eb1f9f3e42f11817f66d30
LorhanSohaky <lorhansohaky@gmail.com>
2019-08-15 00:53:40 -0300

    Criado visit programa, variavel e comando leia

--- trabalho1/AnalisadorLA/src/la/linguagem/AnalisadorLA/AnalisadorSemantico.java
+++ trabalho1/AnalisadorLA/src/la/linguagem/AnalisadorLA/AnalisadorSemantico.java
@@ -1,11 +1,58 @@
 package la.linguagem.AnalisadorLA;
 
 import la.linguagem.ANTLR.laBaseVisitor;
+import la.linguagem.ANTLR.laParser;
+import la.linguagem.ANTLR.laParser.CmdLeiaContext;
+import la.linguagem.ANTLR.laParser.IdentificadorContext;
+import la.linguagem.ANTLR.laParser.VariavelContext;
 
 public class AnalisadorSemantico extends laBaseVisitor {
 
 	SaidaParser saida = SaidaParser.getInstance();
 
 	PilhaDeTabelas pilhaDeTabelas = new PilhaDeTabelas();
+
+	@Override
+	public Void visitPrograma(laParser.ProgramaContext ctx) {
+		pilhaDeTabelas.empilhar(new TabelaDeSimbolos("global"));
+
+		super.visitPrograma(ctx);
+
+		pilhaDeTabelas.desempilhar();
+		return null;
+	}
+
+	@Override
+	public Void visitVariavel(VariavelContext ctx) {
+
+		String tipo = ctx.tipo().getText();
+		tipo = tipo.startsWith("^") ? tipo.substring(1) : tipo;
+
+		if (!tipo.equals("literal") && !tipo.equals("inteiro") && !tipo.equals("real") && !tipo.equals("logico")
+				&& !pilhaDeTabelas.existeSimbolo(tipo)) {
+			saida.println(
+					"Linha " + ctx.tipo().getStart().getLine() + ": tipo " + ctx.tipo().getText() + " nao declarado");
+		}
+
+		for (IdentificadorContext variavel : ctx.identificadores) {
+			pilhaDeTabelas.topo().adicionarSimbolo(variavel.getText(), tipo);
+		}
+
+		return null;
+
+	}
+
+	@Override
+	public Void visitCmdLeia(CmdLeiaContext ctx) {
+
+		for (IdentificadorContext variavel : ctx.identificadores) {
+			if (!pilhaDeTabelas.existeSimbolo(variavel.getText())) {
+				saida.println("Linha " + variavel.getStart().getLine() + ": identificador " + variavel.getText()
+						+ " nao declarado");
+			}
+		}
+
+		return null;
+	}
 
 }
