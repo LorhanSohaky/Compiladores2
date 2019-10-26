4f82b9df59efab2f80739f47e6e98cef5418ba21
LorhanSohaky <lorhansohaky@gmail.com>
2019-10-26 19:02:34 -0300

    Criado script para testar o compilador

---
+++ trabalho2/testGrammar.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+./generateGrammar.sh
+
+./generateJar.sh
+
+quantidade_de_testes=$(find casosDeTeste/entrada/*.md | wc -l)
+
+acertos=0
+erros=0
+
+for i in $(seq 0 $(expr $quantidade_de_testes - 1) ); do
+    java -jar meu_compilador.jar casosDeTeste
+    diff casosDeTeste/saida/$i.tex saidaProduzida/$i.tex -q
+    if [ $? -eq 1 ]
+    then
+        erros=$(expr $erros + 1)
+    else
+        acertos=$(expr $acertos + 1)
+    fi
+done
+
+media=$(echo 'scale=2;' $acertos / $quantidade_de_testes | bc)
+
+echo '----! Resultado !----'
+echo 'Acertos:' $acertos
+echo 'Erros:' $erros
+echo 'Media:' $media
+
+
+
